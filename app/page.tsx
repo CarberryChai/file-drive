51edd8b7409edf9b4aeed4b1a09fdc9625e38c08
Changlin <CarberryChai@gmail.com>
2024-03-04 12:37:51 +0800

    Add file creation functionality

--- app/page.tsx
+++ app/page.tsx
@@ -1,9 +1,19 @@
-import Image from 'next/image'
+'use client'
+import { Button } from '@/components/ui/button'
+import { api } from '@/convex/_generated/api'
+import { useMutation } from 'convex/react'
 
 export default function Home() {
+  const createFile = useMutation(api.files.createFile)
   return (
     <main className='flex min-h-screen flex-col items-center justify-between p-24'>
-      hello world
+      <Button
+        onClick={() => {
+          createFile({ name: 'hello form chai' })
+        }}
+      >
+        create new file
+      </Button>
     </main>
   )
 }
