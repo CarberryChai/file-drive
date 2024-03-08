57f92b6db2ec2708fdf4e5b9aa4bb64b36e60c35
Changlin <CarberryChai@gmail.com>
2024-03-08 20:48:52 +0800

    Add file upload functionality to UploadForm component

--- components/Upload.tsx
+++ components/Upload.tsx
@@ -1,44 +1,58 @@
 'use client'
+import { Button } from '@/components/ui/button'
+import {
+  Dialog,
+  DialogContent,
+  DialogFooter,
+  DialogHeader,
+  DialogTitle,
+  DialogTrigger,
+} from '@/components/ui/dialog'
+import { api } from '@/convex/_generated/api'
 import { zodResolver } from '@hookform/resolvers/zod'
+import { useMutation } from 'convex/react'
 import { useForm } from 'react-hook-form'
 import { z } from 'zod'
 import {
   Form,
   FormControl,
-  FormDescription,
   FormField,
   FormItem,
   FormLabel,
   FormMessage,
 } from './ui/form'
 import { Input } from './ui/input'
-import { Button } from '@/components/ui/button'
-import {
-  Dialog,
-  DialogContent,
-  DialogDescription,
-  DialogFooter,
-  DialogHeader,
-  DialogTitle,
-  DialogTrigger,
-} from '@/components/ui/dialog'
-import { Label } from '@/components/ui/label'
 
 const formScheme = z.object({
   title: z.string().min(2).max(200),
-  file: z.any(),
+  file: z
+    .custom<FileList>(value => value instanceof FileList, 'Required')
+    .refine(files => files.length > 0, 'Required'),
 })
 
 export default function UploadForm() {
+  const generateUploadUrl = useMutation(api.files.generateUploadUrl)
+  const createFile = useMutation(api.files.createFile)
   const form = useForm<z.infer<typeof formScheme>>({
     resolver: zodResolver(formScheme),
     defaultValues: {
       title: '',
+      file: undefined,
     },
   })
 
-  function onSubmit(values: z.infer<typeof formScheme>) {
-    console.log(values)
+  const fileRef = form.register('file')
+
+  async function onSubmit(values: z.infer<typeof formScheme>) {
+    const url = await generateUploadUrl()
+    const result = await fetch(url, {
+      method: 'POST',
+      headers: { 'Content-Type': values.file[0].type },
+      body: values.file[0],
+    })
+    const { storageId } = await result.json()
+    //TODO: orgId
+    await createFile({ name: values.title, fileId: storageId, orgId: '1' })
   }
 
   return (
@@ -51,7 +65,10 @@ export default function UploadForm() {
           <DialogTitle>Upload File</DialogTitle>
         </DialogHeader>
         <Form {...form}>
-          <form onSubmit={form.handleSubmit(onSubmit)}>
+          <form
+            className='flex flex-col gap-4'
+            onSubmit={form.handleSubmit(onSubmit)}
+          >
             <FormField
               control={form.control}
               name='title'
@@ -61,9 +78,6 @@ export default function UploadForm() {
                   <FormControl>
                     <Input placeholder='title' {...field} />
                   </FormControl>
-                  <FormDescription>
-                    This is the title of your file.
-                  </FormDescription>
                   <FormMessage />
                 </FormItem>
               )}
@@ -75,11 +89,8 @@ export default function UploadForm() {
                 <FormItem>
                   <FormLabel>File</FormLabel>
                   <FormControl>
-                    <Input type='file' {...field} />
+                    <Input type='file' {...fileRef} />
                   </FormControl>
-                  <FormDescription>
-                    Click here to upload your file.
-                  </FormDescription>
                   <FormMessage />
                 </FormItem>
               )}
--- convex/files.ts
+++ convex/files.ts
@@ -2,9 +2,18 @@ import { ConvexError, v } from 'convex/values'
 import { mutation, query } from './_generated/server'
 import { getUser } from './users'
 
+export const generateUploadUrl = mutation(async ctx => {
+  const identity = await ctx.auth.getUserIdentity()
+  if (!identity) {
+    throw new ConvexError('You must be logined in to upload files.')
+  }
+  return await ctx.storage.generateUploadUrl()
+})
+
 export const createFile = mutation({
   args: {
     name: v.string(),
+    fileId: v.id('_storage'),
     orgId: v.string(),
   },
   async handler(ctx, args) {
@@ -16,6 +25,7 @@ export const createFile = mutation({
     await ctx.db.insert('files', {
       name: args.name,
       orgId: args.orgId,
+      fileId: args.fileId,
     })
   },
 })
